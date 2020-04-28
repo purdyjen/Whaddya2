54ae1e7d439f7eafbee79e5d10d5a79048b2d6a1
Jennifer Moundanos <jmoundanos@gmail.com>
2020-04-27 20:37:02 -0500

    add server code

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const app = express();
+//const routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//app.use(routes);
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/whaddya2",{useNewUrlParser: true}
+ )
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
