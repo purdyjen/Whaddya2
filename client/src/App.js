7162da0157eeca97f599517bb11b6bfa4d271d1e
purdyjen <purdyjen@me.com>
2020-05-02 10:24:06 -0500

    genres cards and images

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Profile from "./pages/Profile";
-import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Options from "./pages/Options";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -12,7 +12,7 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={Signup} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/options" component={Options} />
           <Route component={NoMatch} />
