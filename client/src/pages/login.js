8331b8d1f7075ec4f09b6988dcb9bc70a4854b15
purdyjen <purdyjen@me.com>
2020-05-02 12:40:25 -0500

    edited app and login pages

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -4,13 +4,15 @@ import { Col, Row, Container } from "../components/Grid";
 //import Jumbotron from "../components/Jumbotron";
 // import API from "../utils/;
 import AuthCard from "../components/AuthCard";
+import Header from "../components/Header";
+
 function Login() {
     return (
       <Container fluid>
         <Row>
           <Col size="md-12">
-            {/* <AuthCard/> */}
-          
+            <Header />
+        <AuthCard />
           </Col>
         </Row>
       </Container>
