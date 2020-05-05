3b9abc6a948001c4e790125c829443dc5274dbff
purdyjen <purdyjen@me.com>
2020-05-05 14:26:25 -0500

    working on genre card rendering

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,55 @@ import cards from "./cards.json";
 // }
 class App extends Component {
 
+  state = {
+    cards,
+    selected: false,
+    selectedgenres: []
+  }
+
+  clickedCard = id => {
+    var state = $(this).attr("data-state");
+    // select an unselected icon
+    if (state === "default") {
+      var selectedVal = $(this).attr("data-selected");
+      $(this).attr("src", selectedVal);
+      console.log(selectedVal);
+
+      $(this).addClass("selected");
+      $(this).attr("data-state", "selected");
+      $(this).removeClass("default");
+
+      // ! verifying
+      var state = $(this).attr("data-state");
+      console.log(state);
+
+      // logic tied to the user's current location
+      // if the user is at genres
+      if (window.location.href.includes("genres")) {
+        // save the users genre selection to a variable
+        console.log("done");
+
+        genre = $(this).attr("name");
+        // genreChoice.push(choice)
+        console.log(genreChoice);
+
+        // pass the user's choice to the getMovies function
+        getMovies(genre);
+        // if the user is at platforms
+      } else if (window.location.href.includes("platforms")) {
+        platformChoice = $(this).attr("name");
+        console.log(platformChoice);
+      }
+      // deselect a selected icon
+    } else if (state === "selected") {
+      var defaultVal = $(this).attr("data-default");
+      $(this).attr("src", defaultVal);
+      console.log(defaultVal);
+      $(this).addClass("default");
+      $(this).attr("data-state", "default");
+      $(this).removeClass("selected");
+    }
+  } //close clickedCard
   render () {
     return (
     <div className="App">
