5387930cdb5bbf53e114b59507eacf08258f6187
Jennifer Moundanos <jmoundanos@gmail.com>
2020-05-06 13:07:09 -0500

    add database

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 // import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
+
 
 //import API from "../utils/API";
-//import { Col, Row, Container } from "react-grid-system";
+
 
 import AuthCard from "../components/AuthCard";
 
