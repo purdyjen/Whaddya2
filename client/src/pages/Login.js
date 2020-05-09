6126904413b34e9dbf30176b6d373a1f52b28900
purdyjen <purdyjen@me.com>
2020-05-09 10:09:44 -0500

    fixed login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from "react";
+
+// import { Link } from "react-router-dom";
+
+//import API from "../utils/API";
+
+
+import AuthCard from "../components/AuthCard";
+
+class Login extends Component {
+render() {
+    return (
+     
+      <AuthCard />
+    
+
+    );
+  }
+}
+  export default Login;   
