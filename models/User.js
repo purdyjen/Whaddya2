4c1abb25b8795dc90ffed4d768ace7d666ab75fc
purdyjen <purdyjen@me.com>
2020-05-09 13:18:26 -0500

    added to User model

--- models/User.js
+++ models/User.js
@@ -4,7 +4,18 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, required: true },
   email: { type:String, required: false},
-  friends: { type:Array, required: false}
+  sentRequest:[{
+    username: {type: String, default: ""}
+    }],
+    request: [{
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    username: {type: String, default: ""}
+    }],
+    friendsList: [{
+    friendId: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    friendName: {type: String, default: ""}
+    }],
+    totalRequest: {type: Number, default:0}
 });
 
 const User = mongoose.model("User", UserSchema);
