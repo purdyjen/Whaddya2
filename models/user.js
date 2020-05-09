2aa763ccc7682820c8e685580a484fd8e8bae4a1
Jennifer Moundanos <jmoundanos@gmail.com>
2020-05-09 10:52:02 -0500

    change model files

--- models/user.js
+++ models/user.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String, required: true}
+    username: {type: String, required: true},
+    email: { type:String, required: true},
+    friends: { type:Array, required: true}
 })
 
 const User = mongoose.model("User", userSchema);
