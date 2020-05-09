bc565e033488d4588b9248750df1e90d1d051ef8
Jennifer Moundanos <jmoundanos@gmail.com>
2020-05-09 10:34:12 -0500

    add npm packages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 //const routes = require("./routes");
+const dotenv = require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
