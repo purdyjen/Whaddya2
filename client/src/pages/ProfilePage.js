0b358fd4f1b0df562d721842a9929bc87152b552
Jennifer Moundanos <jmoundanos@gmail.com>
2020-05-14 18:57:19 -0500

    added socket.io

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,41 +1,48 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
 import API from "../utils/API";
 import Genres from "../components/Genres";
 import LogoutBtn from "../components/LogoutBtn";
 import Friends from "../components/Friends";
 import io from "socket.io-client";
 
+
 class ProfilePage extends Component {
   constructor(props){
     super(props);
-  this.state ={
-    users: [], 
-    message: "yes",
-    messages: []
-  };
-  this.socket = io("localhost:8080");
-
-  this.socket.on("RECEIVE_MESSAGE", function(data){
-    addMessage(data);
-  });
-
-  const addMessage = data => {
-    console.log(data);
-    this.setState({messages: [...this.state.messages, data]});
-    console.log(this.state.messages);
+    this.state ={
+      users: [],
+      message: ''
+    
   }
-  this.sendMessage = ev => {
+  this.socket = io();
+
+  this.socket.on('RECEIVE_MESSAGE', function(data){
+    //addMessage(data);
+    console.log(data);
+});
+
+// const addMessage = data => {
+//     console.log(data);
+//     //this.setState({messages: [...this.state.messages, data]});
+//     //console.log(this.state.messages);
+// };
+
+this.sendMessage = ev => {
     ev.preventDefault();
-    this.socket.emit("SEND_MESSAGE", {
-      message: this.state.message
+    this.socket.emit('SEND_MESSAGE', {
+        author: this.state.username,
+        message: this.state.message
     })
     this.setState({message: ''});
-  }
 }
 
-  addFriend = () => {
-    console.log("friend click");
-  }
+} 
+
+  // addFriend = () => {
+  //   console.log("friend click");
+  //   this.setState({message: true});
+  //   //console.log(message);
+  // }
 
   componentDidMount(){
     this.loadUsers();
@@ -57,13 +64,16 @@ render() {
         <LogoutBtn />
         <Genres />
         <h2>Friends</h2>
+        
         {this.state.users.length ? (
-            <div onClick={this.sendMessage}>
+            <div>
             {this.state.users.map((user) =>(
               <Friends key={user._id} >
-              {user.username}
-
-            </Friends>
+                {user.username}
+                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+                <br/>
+                <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
+              </Friends>
             ))}
             </div>
           ):(
--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const app = express();
 const routes = require("./routes");
 const socket = require("socket.io");
 
+server = app.listen(3001, function(){
+  console.log("server on port 3001");
+})
+io = socket(server);
+
+io.on('connection', (socket) =>{
+  console.log(socket.id);
+
+    socket.on('SEND_MESSAGE', function(data){
+      io.emit('RECEIVE_MESSAGE', data);
+  })
+})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
@@ -17,28 +30,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 
-// io.on("connection", (client) => {
-//   // client.on('subscribeToTimer', (interval) => {
-//   //   console.log('client is subscribing to timer with interval ', interval);
-//   //   setInterval(() => {
-//   //     client.emit('timer', new Date());
-//   //   }, interval);
-//   // });
-// });
-// const port = 8000;
-// io.listen(port);
-// console.log("io listening on port", port);
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/whaddya2",{useNewUrlParser: true}
   )
-server = app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
-  });
-  io = socket(server);
-io.on('connection', (socket) =>{
-  console.log(socket.id);
-  socket.on("SEND_MESSAGE", function(data){
-    io.emit("RECEIVE_MESSAGE", data);
-  })
-});
+// app.listen(PORT, () => {
+//     console.log(`🌎 ==> API server now on port ${PORT}!`);
+//   });
+  
