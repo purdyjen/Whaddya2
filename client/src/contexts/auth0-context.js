b4396acadee087050947cefd31e262a56fc4efb4
Jennifer Moundanos <jmoundanos@gmail.com>
2020-05-15 11:53:14 -0500

    changed redirect_uri

--- client/src/contexts/auth0-context.js
+++ client/src/contexts/auth0-context.js
@@ -17,7 +17,7 @@ export class Auth0Provider extends Component {
     config = {
         domain: process.env.REACT_APP_AUTH0_DOMAIN,
         client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
-        redirect_uri: "http://localhost:3000/profile"
+        redirect_uri: "https://whaddya2.herokuapp.com/profile"
     };
     componentDidMount() {
         this.initializeAuth0();
