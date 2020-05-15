a6948f32257b83001cb38f06ef6e9ce36ab5d6c5
Jennifer Moundanos <jmoundanos@gmail.com>
2020-05-15 11:12:46 -0500

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const routes = require("./routes");
 //const socket = require("socket.io");
